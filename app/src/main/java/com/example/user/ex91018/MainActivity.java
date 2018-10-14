e0e1374e01c60f3f82aa6426f8156d7766965a4e
NitzanDromi <nd0389@bs.amalnet.k12.il>
2018-10-14 12:36:46 +0300

    first2

--- app/src/main/java/com/example/user/ex91018/MainActivity.java
+++ app/src/main/java/com/example/user/ex91018/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.user.ex91018;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.Toast;
 
@@ -23,6 +27,19 @@ public class MainActivity extends AppCompatActivity {
         rbha= (RadioButton) findViewById(R.id.rbha);
         etfirst=(EditText) findViewById(R.id.etfirst);
         etdm=(EditText) findViewById(R.id.etdm);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id=item.getItemId();
+        if (id==R.id.menucreds){
+            Toast.makeText(this, "Creator - Nitzan Dromi", Toast.LENGTH_LONG).show();
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 
